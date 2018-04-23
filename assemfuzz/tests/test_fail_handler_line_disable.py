1494db7a1a492098aeaa7a5ceac2f9a06fcc6b8b
Joshua Achermann <joshua.achermann@gmail.com>
2018-04-23 16:38:07 +1000

    Added a new test that fails

---
+++ assemfuzz/tests/test_fail_handler_line_disable.py
@@ -0,0 +1,34 @@
+"""
+generates long strings of valid hack assembly and compares against a
+reference assembler
+
+Copyright (C) 2017  Joshua Achermann
+
+assem-fuzz is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+email: joshua.achermann@gmail.com
+"""
+#!/bin/python3
+from common import on_win, fuzz
+
+def their_cond(err):
+    return None
+
+def test_fuzz():
+    """Perform the fuzzing with the given options"""
+    on_windows = on_win()
+    fuzz(True, on_windows, their_cond)
+
+if __name__ == "__main__":
+    test_fuzz()
